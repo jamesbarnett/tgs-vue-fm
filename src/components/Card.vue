5d48f6f196f81ac24ab8de069f9ebafd1c07561f
Jim Barnett <james.barnett.jr@gmail.com>
2024-07-20 11:18:47 -0500

    Completed first pass at mobile

---
+++ src/components/Card.vue
@@ -0,0 +1,53 @@
+<script setup>
+  const props = defineProps([
+    "accent",
+    "image",
+    "student",
+    "status",
+    "summary",
+    "testimonial"
+  ])
+</script>
+<template>
+  <article class="card flow rounded-xl p-8 border border-solid mx-auto w-screen mb-6">
+    <div class="flex gap-8">
+      <img :src=props.image class="inline rounded-full w-[28px] h-[28px]" />
+      <div class="flex flex-1 flex-col">
+        <h1 class="inline text-[0.8125rem] font-semi-bold tracking-tight">{{props.student}}</h1>
+        <h2 class="inline text-[0.625rem] tracking-tight">{{props.status}}</h2>
+      </div>
+    </div>
+    <p class="font-semibold text-[1.125rem] tracking-tight leading-tight pb-8">{{props.summary}}</p>
+    <p class="testimonial text-[0.75rem] tracking-tight">{{props.testimonial}}</p>
+  </article>
+</template>
+<style scoped>
+  article {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .card[data-accent="violet"] {
+    background: var(--clr-primary-violet);
+    color: var(--clr-neutral-gray);
+  }
+
+  .card[data-accent="grayblue"] {
+    background: var(--clr-primary-grayblue);
+    color: var(--clr-primary-white);
+  }
+
+  .card[data-accent="white"] {
+    background: var(--clr-primary-white);
+    color: var(--clr-primary-grayblue);
+  }
+
+  .card[data-accent="blackblue"] {
+    background: var(--clr-primary-blackblue);
+    color: var(--clr-primary-white);
+
+    .testimonial {
+      color: var(--clr-neutral-gray);
+    }
+  }
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        barlow: "'Barlow', sans",
+      },
+      colors: {
+        primary: {
+          violet: "var(--clr-primary-violet)",
+          grayblue: "var(--clr-primary-grayblue)",
+          blackblue: "var(--clr-primary-blackblue)",
+        },
+        neutral: {
+          gray: "var(--clr-neutral-gray)",
+          grayblue: "var(--clr-neutral-grayblue)",
+        },
+      },
+    },
   },
   plugins: [],
 }
